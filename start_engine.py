1404513f874957c3b08b1d3f227d0728fd916b28
Sandip Pawar <sandip.pawar@globant.com>
2023-04-21 05:59:33 +0530

    remove unneccessary code

--- start_engine.py
+++ start_engine.py
@@ -25,21 +25,6 @@ option_wizard.update_to_latest_v3()
 record = option_wizard.find_cheapest_options(n=15)
 option_wizard.send_to_telegram(cheapest_records=record['cheapest_options'], today=record['day'])
 # option_wizard.place_orders(cheapest_records=record['cheapest_options'], trade_date=record['day'])
-# start_date=pd.to_datetime(date(2023,3,15))#  this will include Feb month expiry  data
-# end_date=pd.to_datetime(date(2023,3,16))# this will include March month expiry data
-# option_wizard.download_historical_v3(start_date,end_date)
-# option_wizard.update_current_vs_prev_two_months(start_date=start_date,end_date=end_date)
-# option_wizard.download_historical(start_date=start_date,end_date=end_date)
-
-# print(option_wizard.map_symbol_name('LTIM'))
-# option_wizard.get_oneday_options_history('NIITTECH','CE', date.today()-timedelta(days=1),option_wizard.get_expiry(2023,3),4300)
-# df=option_wizard.get_month_fut_history('RELIANCE',2023,3)
-# print(df.shape)
-# start_month_date=pd.to_datetime(date(2022,11,1))
-# end_month_date=pd.to_datetime(date.today())
-# days=(end_month_date-start_month_date).days
-# of_date=pd.to_datetime(date(2022,10,31))
-
 # option_wizard.download_options_for_pnl(back_test=False)
 # portfolio = option_wizard.get_portfolio_pnl()
 # if portfolio['total_capital'] > 0:
